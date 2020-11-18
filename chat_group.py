db3daa9f7d480086d2842a806ce05e66fe1d6b68
Pieter Patonedi <patonedipieter@gmail.com>
2020-11-18 21:52:17 +0100

    testing 1

--- chat_group.py
+++ chat_group.py
@@ -4,14 +4,14 @@ import datetime
 import os
 
 
-token = " "# isi token grup buat tes
+token = "iis96ihAcH7wPoTZQpBymGCcz1JTIVSv75USzXWBoYB"# isi token grup buat tes
 
 
 kalimat = input(">> ")
 
 LINE_ACCESS_TOKEN = token
 url = "https://notify-api.line.me/api/notify"
-msg = {"message":str(kalimat)} #message    
+msg = {"message":str(kalimat)} #message
 LINE_HEADERS = {'Content-Type':'application/x-www-form-urlencoded',"Authorization":"Bearer "+LINE_ACCESS_TOKEN}
 session = requests.Session()
 resp =session.post(url, headers=LINE_HEADERS, data=msg)
